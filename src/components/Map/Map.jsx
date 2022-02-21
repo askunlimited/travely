7f557549102ac818dcb1ab89963639e698856660
Adika King <SlntCoda>
2022-02-21 13:00:13 +0100

    Setup the Google Map API and Listing section style

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlined  from '@material-ui/icons/LocationOnOutlined';
+
+import { Rating } from '@material-ui/lab';
+
+
+import useStyles from './styles';
+import googleMapReact from 'google-map-react';
 
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+
+
+  const coordinates = { lat: 0, lng: 0 };
   return (
-    <div>Map</div>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyBzUx-pCnVX6DAX7uMH56dwJIYaZv_zcDU' }}
+        defaultCenter={{}}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      
+      >
+
+      </GoogleMapReact>
+       
+    </div>
   )
 }
 
